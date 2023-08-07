3983604ec44568d2ef628d1710b5a770377ef191
Deepak Chaudhari <49471265+deepak14ri@users.noreply.github.com>
2023-08-07 15:14:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const UserModel = require('./Models/User')
+const ItemModel = require('./Models/Item')
 
 const app = express()
 app.use(cors(
